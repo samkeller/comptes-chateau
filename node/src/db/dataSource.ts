153971469b2dccfb731e8f27ca306a7a964c8b50
samkeller <keller.samuel1@gmail.com>
2026-02-01 17:21:01 +0100

    Fix

--- node/src/db/dataSource.ts
+++ node/src/db/dataSource.ts
@@ -6,7 +6,7 @@ const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
    type: 'postgres',
-   url: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?options=-c%20search_path=${process.env.DB_SCHEMA}`, 
+   url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?options=-c%20search_path=${process.env.DB_SCHEMA}`, 
    host: process.env.DB_HOST,
    port: port,
    username: process.env.DB_USER,
