8372af5c748774637dc4a2d3c3893d3d54a34b97
samkeller <keller.samuel1@gmail.com>
2026-02-04 10:45:37 +0100

    Fix

--- react/src/components/datatableBodys/ColoredLabel.tsx
+++ react/src/components/datatableBodys/ColoredLabel.tsx
@@ -8,16 +8,16 @@ interface ColoredLabelProps {
 
 export function ColoredLabel({ data }: ColoredLabelProps) {
     return (
-        <div style={{ display: "inline-flex", alignItems: "center" }}>
+        <div className="flex align-items-center">
             <span
-                aria-hidden="true"
-                style={{
-                    width: "0.5rem",
-                    height: "0.5rem",
-                    borderRadius: "0.5rem",
-                    background: data.color,
-                    display: "inline-block",
-                }}
+            aria-hidden="true"
+            style={{
+                width: "0.5rem",
+                height: "0.5rem",
+                borderRadius: "0.5rem",
+                background: data.color,
+                display: "inline-block",
+            }}
             />
             <span className="ml-1">{data.label}</span>
         </div>
