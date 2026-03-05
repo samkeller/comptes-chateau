75d361b5eb35aa3c1a8ac4987aa043d51cfed98c
samkeller <keller.samuel1@gmail.com>
2026-03-05 16:20:38 +0100

    Fix style

--- react/src/pages/index/Index.tsx
+++ react/src/pages/index/Index.tsx
@@ -6,6 +6,7 @@ import { DashboardOverview } from "../../interfaces/DashboardOverview";
 import { ProgressSpinner } from "primereact/progressspinner";
 import { toMonetaryAmount } from "../../utils/NumberUtils";
 import MonthlyDashboard from "./monthlyDashboard/MonthlyDashboard";
+import { Tooltip } from "primereact/tooltip";
 
 export default function Index() {
     const [overview, setOverview] = useState<DashboardOverview | null>(null);
@@ -44,6 +45,38 @@ export default function Index() {
         return lastDay.getDate() - today.getDate();
     }, []);
 
+    const IndicatorCard = ({
+        title,
+        value,
+        description,
+        tooltipText,
+        className
+    }: {
+        title: string;
+        value: string;
+        description: string,
+        tooltipText: string,
+        className?: string
+    }) => (
+
+        <Card
+            title={<span className="flex align-content-center">
+                {title}
+                <Tooltip target=".title-explaining" />
+                <i
+                    className="title-explaining pi pi-info-circle ml-2 my-auto text-sm text-500"
+                    data-pr-tooltip={tooltipText}
+                />
+            </span>}
+            className="h-full"
+        >
+            <h2 className={`text-4xl font-bold ${className ?? ""}`} >
+                {value}
+            </h2>
+            <div className="text-500 mt-2">{description}</div>
+        </Card>
+    )
+
     return (
         <PageTemplate pageTitle="Dashboard">
             {loading && (
@@ -55,23 +88,23 @@ export default function Index() {
             {!loading && overview && (
                 <div className="grid">
                     <div className="col-12 md:col-4">
-                        <Card title="Solde actuel" className="h-full">
-                            <div className={`text-4xl font-bold ${getBalanceClass(overview.currentBalance)}`}>
-                                {toMonetaryAmount(overview.currentBalance)}
-                            </div>
-                            <div className="text-500 mt-2">Opérations validées uniquement</div>
-                        </Card>
+                        <IndicatorCard
+                            title="Solde actuel"
+                            value={toMonetaryAmount(overview.currentBalance)}
+                            description="Opérations validées uniquement"
+                            tooltipText="Prend en compte uniquement les opérations 'validées' dont les natures de dépenses sont liées au compte en banque. eprésente l'état actuel du compte."
+                            className={getBalanceClass(overview.currentBalance)}
+                        />
                     </div>
-
                     <div className="col-12 md:col-4">
-                        <Card title="Solde prévisionnel" className="h-full">
-                            <div className={`text-4xl font-bold ${getBalanceClass(overview.forecastBalance)}`}>
-                                {toMonetaryAmount(overview.forecastBalance)}
-                            </div>
-                            <div className="text-500 mt-2">Toutes les opérations (validées + à venir)</div>
-                        </Card>
+                        <IndicatorCard
+                            title="Solde prévisionnel"
+                            value={toMonetaryAmount(overview.forecastBalance)}
+                            description="Toutes les opérations (validées + à venir)"
+                            tooltipText="Prend en compte uniquement toutes les opérations ('validées' ou non) dont les natures de dépenses sont liées au compte en banque."
+                            className={getBalanceClass(overview.forecastBalance)}
+                        />
                     </div>
-
                     <div className="col-12 md:col-4">
                         <Card title="Dépenses du mois" className="h-full">
                             <div className="text-4xl font-bold text-900">
